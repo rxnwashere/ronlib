966150fecc70df45ce575795dde4533501d124f1
Rxn <79046280+rxnwashere@users.noreply.github.com>
2024-09-13 14:28:12 +0200

    First version of the webpage format of RonLib

---
+++ main.js
@@ -0,0 +1,11 @@
+alert("Welcome to Ron Lib! I'm still developing the webpage format of the repository. Please be patient while I'm adding commits and changes to the webpage.")
+
+function godownbuttonclick () {
+    document.getElementById("godown").style.display = "none";
+}
+
+function mousehover () {
+    document.getElementById("apuntsm7") = alert("Documentació projecte sintesis 2024")
+}
+
+/* STILL WORKING ON THE PAGE'S SCRIPT */
